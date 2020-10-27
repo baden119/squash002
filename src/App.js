e553e554e71468417c2d23a8376ce488cbc0d1f7
baden allen <baden.j.allen@gmail.com>
2020-10-27 18:14:13 +1100

    Trying to convert api data from app.js state into something victorycharts can use. it. is. not. going. well.

--- src/App.js
+++ src/App.js
@@ -11,8 +11,46 @@ class App extends Component {
       subreddit: '',
       after_date: '',
       before_date: '',
-      api_data: []
+      api_data: [
+              {
+                "doc_count": 6736,
+                "key": 1577836800
+              },
+              {
+                "doc_count": 8000,
+                "key": 1580515200
+              },
+              {
+                "doc_count": 10058,
+                "key": 1583020800
+              },
+              {
+                "doc_count": 10600,
+                "key": 1585699200
+              },
+              {
+                "doc_count": 7202,
+                "key": 1588291200
+              },
+              {
+                "doc_count": 5397,
+                "key": 1590969600
+              },
+              {
+                "doc_count": 7457,
+                "key": 1593561600
+              },
+              {
+                "doc_count": 5632,
+                "key": 1596240000
+              },
+              {
+                "doc_count": 4696,
+                "key": 1598918400
+              }
+            ]
   }
+
 
   inputData = (input_data) => {
      this.setState({[input_data.name]: input_data.value});
--- src/components/CreateGraph.js
+++ src/components/CreateGraph.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import { VictoryChart, VictoryLine, VictoryTheme } from "victory";
 
 class CreateGraph extends Component {
 
   // timeTravel = () => {
   // }
 
+  // state = {
+  //   plot_data : [this.props.api_Data]
+  // }
+
+    // componentDidMount() {
+    //   for (const [index, value] of this.props.apiData.entries()) {
+    //     let data_point = {"date": moment.unix(value.key).format("MMMM, YYYY"), "submissions": value.doc_count};
+    //     this.setState(state=>{
+    //       this.state.plot_data.push(data_point)
+    //     })}
+    //     console.log("data added to state")
+    //  }
+
+  const dataFormatter = function() {
+    console.log("DATAFORMATTER!!!!")
+  }
+
   render() {
     return (
       <div>
+        {console.log("rendering div")}
+        {dataFormatter}
       {/* <button onClick={ () => this.timeTravel()} > Create Graph </button> */}
-      {this.props.apiData.map((item) => (
+      {/* {this.props.apiData.map((item) => (
         <p>{(moment.unix(item.key).format("MMMM, YYYY"))} | {item.doc_count}</p>
-      ))}
+      ))} */}
+      {/* Victory Chart */}
+      {/* console.log({this.state.plot_data})
+       <div style={{ maxWidth: "800px", margin: "0 auto", backgroundColor: "lightgrey" }}>
+         <VictoryChart theme={VictoryTheme.material} width={800} height={400}>
+           <VictoryLine data={this.state.plot_data} />
+
+         </VictoryChart>
+        </div> */}
       </div>
     );
   }
 
 }
+
 
 export default CreateGraph;
